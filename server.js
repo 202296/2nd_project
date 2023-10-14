e8aba9278c417098d030ade593a3abbded3f2e93
202296 <mouhamedkoko996@gmail.com>
2023-10-14 23:19:56 +0000

    start

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const express = require('express');
-const dbConnect = require('./src/configs/connectDB');
+const cors = require('cors');
+const { serve, setup } = require('swagger-ui-express');
 const app = express();
+app.use(cors());
+const bodyParser = require('body-parser')
+// Use the JavaScript Swagger definition
+const swaggerDefinition = require('./swagger.json');
+const dbConnect = require('./src/configs/connectDB');
 const dotenv = require('dotenv').config();
-const PORT = process.env.PORT || 5000;
-const authRouter = require('./src/routes/authRoute');
+const userRouter = require('./src/routes/authRoute');
 
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+// Serve Swagger UI at /docs
+app.use('/api-docs', serve, setup(swaggerDefinition));
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan')
+
 dbConnect()
-
-app.use(morgan("dev"))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(cookieParser())
-
-
-app.use('/api/user', authRouter);
-
-app.listen(PORT, ()=>{
-    console.log(`Server is running at PORT ${PORT}`);
-})
+app.use(bodyParser.urlencoded({extended: false}));
+app.use("/api/user", userRouter);
+const port = 5000;
+app.listen(process.env.PORT || port);
+console.log(`Web Server is listening at: ${process.env.PORT || port}`);
