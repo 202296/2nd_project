4948a6b32dfc749f25c8862e87a0fd629dc17eff
202296 <mouhamedkoko996@gmail.com>
2023-10-19 12:19:15 +0000

    password

--- src/models/authModel.js
+++ src/models/authModel.js
@@ -45,8 +45,27 @@ var userSchema = new mongoose.Schema(
         type: String,
         default: "employee",
       },
+      refreshToken: {
+        type: String,
+      },
     },
+    {
+      timestamps: true,
+    }
   );
+
+  userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+      next();
+    }
+    const salt = await bcrypt.genSaltSync(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  });
+  
+  userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  };
 
 //Export the model
 module.exports = mongoose.model('User', userSchema);
